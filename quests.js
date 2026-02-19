fd1d41612e5e4d305b3fa46dc6b9d9d166734b51
httpswap <malikovdevx@gmail.com>
2026-02-19 17:08:29 +0500

    lesson 2 scop,var,let

---
+++ quests.js
@@ -0,0 +1,18 @@
+//1
+console.log("5" + 5);
+//2
+console.log("5" - 5);
+//3
+console.log("5" * "2");
+//4
+console.log("10" / 2);
+//5
+console.log(5 + true);
+//6
+console.log("10" - true);
+//7
+console.log("5"-true);
+//8
+console.log(5+null);
+//9
+console.log(5+undefined);
