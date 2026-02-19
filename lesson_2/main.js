ab8006f4d4f2c6d46157aac61348781cac21efa0
hakimbek <hakimbekmrx@gmail.com>
2026-02-19 17:00:02 +0500

    conversion and coercion example 1

---
+++ lesson_2/main.js
@@ -0,0 +1,22 @@
+/*
+55
+0
+?
+5
+6
+10true
+5true
+5
+5
+*/
+
+//example 1
+console.log("5"+5);//1
+console.log("5"-5);//1
+console.log("5"*"2");//0
+console.log("10"/2);//1
+console.log("5"+true);//
+console.log("10"-true);
+console.log("5"-true);
+console.log(5+null);
+console.log(5+undefined);
