dacc0a74e133567bf70868a9b5943ae53ef28395
Shuhrat775 <michchi06@gmail.com>
2026-02-20 17:12:05 +0500

    1,2,3-topshiriq

--- .vscode/app.js
+++ .vscode/app.js
@@ -10,3 +10,49 @@
 // console.log("5"+true) Result: 5true
 // console.log(5+null) Result: 5
 // console.log(5+undefined); Result: NaN
+
+//1-topshiriq
+// let list = [34,13,9,5,14,45,67,72,39,63]
+
+// let jcount = 0
+// let tcount = 0
+
+// for (let i=0; i<list.length; i++){
+//     if (list[i] > 50){
+//         jcount++
+//     } else {
+//         tcount++
+//     }
+// }
+// console.log("Juft son",jcount)
+// console.log("Toq son",tcount)
+
+//2-topshiriq
+ // for (let i = 0; i < list.length; i++) {
+//     if (list[i] % 2 == 0) {
+//         juft += 1
+//     } else {
+//         toq += 1
+//     }
+// }
+// console.log("Juft:", juft)
+// console.log("Toq:", toq)
+
+//3-topshiriq
+// let son = 583274;
+
+// let max = 0;
+
+// while (son > 0) {
+//     let raqam = son % 10;  
+    
+//     if (raqam > max) {
+//         max = raqam;       
+//     }
+    
+//     son = Math.floor(son / 10); 
+// }
+
+// console.log("Eng katta raqam:", max);
+
+//4-topshiriq
