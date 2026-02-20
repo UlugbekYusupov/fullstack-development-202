6ffb10a4a9f06382404e7cfeeee96c1d6ec27d2f
Goldder12 <badiyvzamon007@gmail.com>
2026-02-20 16:46:25 +0500

    Fangi fayl ba 2- uyga vazifa

---
+++ main2.js
@@ -0,0 +1,40 @@
+// let n = 10;
+
+// let juft = [];
+// let toq = [];
+
+// for (let i = 1; i <= n; i++) {
+//     if (i % 2 === 0) {
+//         juft.push(i);
+//     } else {
+//         toq.push(i);
+//     }
+// }
+
+// console.log("Juft sonlar:", juft);
+// console.log("Toq sonlar:", toq);
+
+// let n = 1234;
+// let teskari = 0;
+
+// while (n > 0) {
+//     let oxirgi = n % 10;          
+//     teskari = teskari * 10 + oxirgi;
+//     n = Math.floor(n / 10);       
+// }
+
+// console.log(teskari);
+
+// let n = 1234;
+// let son = 0;
+// let katta = 0;
+
+// while (n > 0) {
+//     let oxirgi = n % 10;          
+//     if (oxirgi > katta) {
+//         katta = oxirgi;             
+//     }
+//     n = Math.floor(n / 10);       
+// }
+
+// console.log(katta);
