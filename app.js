c7d4be571bb3b9faea1d99d6dd0bd5a9f506f23a
Murod2703 <msultonov042@gmail.com>
2026-02-20 17:02:34 +0500

    feat: index.html and app.js

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-console.log("5" + 5);
-console.log("5" - 5);
-console.log("5" * 5);
-console.log("10" / 2);
-console.log(5 + true);
-console.log("10" - true);
-console.log("5" + true);
-console.log(5 + null);
-console.log(5 + undefined);
+// console.log("5" + 5);
+// console.log("5" - 5);
+// console.log("5" * 5);
+// console.log("10" / 2);
+// console.log(5 + true);
+// console.log("10" - true);
+// console.log("5" + true);
+// console.log(5 + null);
+// console.log(5 + undefined);
+
+let numbers = [12, 5, 7, 8];
+
+let juft = 0;
+let toq = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        juft++;
+    } else {
+        toq++;
+    }
+}
+
+console.log("Juft:", juft);
+console.log("Toq:", toq);
