78c02cc0c6f7a40aa2690fbb05e42db11f041246
hakimbek <hakimbekmrx@gmail.com>
2026-02-20 15:54:36 +0500

    conversion and coercion example 3

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
-console.log(a);
-let a = 20;
 
-const username = "Ulugbek";
+/*
+- JS 2 xil phase , bosqichda ishlaydi
+ 1. Memory creation(declaratsiyalar tepega ko'tariladi va xoitradan joy olib undefined qiymati beriladi)
+ 2. Executation phase(kod bajariladi qiymatlari tenglashtiriladi)
 
-const password = String("kjfnbsdkjfnb");
+- var block scope emas u function scope
+
+
+- truthy va falsy degan tushuncha bor JS da.
+  falsy larga quyidagilar kiradi. Yaniy agar o'zgaruvchi qiymati shunga teng bo'lsa false chiqadi:
+    1) false
+    2) 0
+    3) ""
+    4) null
+    5) undefined
+    6) NaN
+
+* */
+
+
+
+console.log(user);
+if(user) {
+    console.log("User yo'q")
+}
+
+var user = "Ali";
+
+
+let number = 34;
+
--- lesson_2/main.js
+++ lesson_2/main.js
@@ -1,14 +1,4 @@
-/*
-55
-0
-?
-5
-6
-10true
-5true
-5
-5
-*/
+
 
 //example 1
 console.log("5"+5);//1
@@ -20,3 +10,4 @@ console.log("10"-true);
 console.log("5"-true);
 console.log(5+null);
 console.log(5+undefined);
+
