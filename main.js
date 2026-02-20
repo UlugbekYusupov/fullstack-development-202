b516cc2fa2e73ba125815cdd785406cd8642c1e8
Goldder12 <badiyvzamon007@gmail.com>
2026-02-20 15:59:59 +0500

    Feruzshoh

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("5" + 5); // "55" - string concatenation
+console.log("5" - 5); // 0 - string is converted to number
+console.log("5" * 5); // 25 - string is converted to number
+console.log("5" / 5); // 1 - string is converted to number
+console.log("5" % 2); // 1 - string is converted to number
+console.log("5" ** 2);  // 25 - string is converted to number
+console.log("5" + "5"); // "55" - string concatenation
+console.log("5" - "5"); // 0 - both strings are converted to numbers        
+console.log("5" * "5"); // 25 - both strings are converted to numbers
+console.log("5" / "5"); // 1 - both strings are converted to numbers
+console.log("5" % "2"); // 1 - both strings are converted to numbers
+console.log("5" ** "2");  // 25 - both strings are converted to numbers 
+console.log(5 + true); // 10 - numbers are added together
+console.log(5 - false); // 0 - numbers are subtracted
+console.log(5 * null);
+console.log(5 / undefined);                                              
