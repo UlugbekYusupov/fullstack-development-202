e39914211f818b895f81e36565d7c3c9c1ad2990
hakimbek <hakimbekmrx@gmail.com>
2026-02-23 10:04:07 +0500

    some notes added

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
+// console.log(user);
+// if(user) {
+//     console.log("User yo'q")
+// }
+//
+// var user = "Ali";
+//
+//
+// let number = 34;
+//
 
-/*
-- JS 2 xil phase , bosqichda ishlaydi
- 1. Memory creation(declaratsiyalar tepega ko'tariladi va xoitradan joy olib undefined qiymati beriladi)
- 2. Executation phase(kod bajariladi qiymatlari tenglashtiriladi)
-
-- var block scope emas u function scope
-
-
-- truthy va falsy degan tushuncha bor JS da.
-  falsy larga quyidagilar kiradi. Yaniy agar o'zgaruvchi qiymati shunga teng bo'lsa false chiqadi:
-    1) false
-    2) 0
-    3) ""
-    4) null
-    5) undefined
-    6) NaN
-
-*/
+// const user = {
+//     name: "Ali",
+//     sayName: function() {
+//         console.log(this.name);
+//     }
+// };
+//
+// user.sayName();
 
 
+// const user = {
+//     name: "Ali",
+//     sayName: function() {
+//         let a = {name:"daw"}
+//         function inner() {
+//             console.log(this.name);
+//         }
+//         inner();
+//     }
+// };
+//
+// user.sayName();
+let name = "Global";
 
-console.log(user);
-if(user) {
-    console.log("User yo'q")
+function a(){
+    let b = "DED";
+    function show() {
+        console.log(this.b);
+    }
 }
 
-var user = "Ali";
-
-
-let number = 34;
-
+show();
