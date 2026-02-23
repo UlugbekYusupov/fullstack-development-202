f1de04a350f5b379ad96ebe22b57506fec457818
Samanc1k1 <samndarshavkatov15@gmail.com>
2026-02-23 08:49:21 +0500

    Misol

--- app.js
+++ app.js
@@ -1,9 +1,70 @@
-console.log("5" + 5);
-console.log("5" - 5);
-console.log("5" * 5);
-console.log("10" / 2);
-console.log(5 + true);
-console.log("10" - true);
-console.log("5" + true);
-console.log(5 + null);
-console.log(5 + undefined);
+// let n = Number(prompt("Son kiriting:"));
+
+// let juftlar = [];
+// let toqlar = [];
+
+// for (let i = 1; i <= n; i++) {
+//     if (i % 2 === 0) {
+//         juftlar.push(i);
+//     } else {
+//         toqlar.push(i);
+//     }
+// }
+
+// console.log("Juft sonlar:", juftlar.join(", "));
+// console.log("Toq sonlar:", toqlar.join(", "));
+
+
+// let n = 1234;
+// let reversed = 0;
+
+// while (n > 0){
+//     let four = n%10;
+//     reversed = reversed * 10 +four;
+//     n = Math.floor(n/10);
+// }
+// console.log(reversed);
+
+
+// let n = 78945;  
+// let max = 0;
+// while (n > 0) {
+//     let digit = n % 10;
+
+//     if (digit > max) {   
+//         max = digit;
+//     }
+
+//     n = Math.floor(n / 10);
+// }
+
+// console.log("Eng katta raqam:", max);
+
+
+// let n = 53827;
+// let sum = 0;
+
+// while (n > 0) {
+
+//     let digit = n % 10;
+//     sum = sum + digit;    
+
+//     n = Math.floor(n / 10);
+// }
+
+// console.log("Raqamlar yig'indisi:", sum);
+
+
+// let n = 53827;
+// let count = 0;
+
+// if (n === 0) {
+//     count = 1;
+// }
+
+// while (n > 0) {
+//     n = Math.floor(n / 10);
+//     count = count + 1; 
+// }
+
+// console.log("Raqamlar soni:", count);
