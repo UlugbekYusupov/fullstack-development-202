15bf3997534c353fde6b16beec3492c30cb326db
Goldder12 <badiyvzamon007@gmail.com>
2026-02-24 12:34:56 +0500

    main3.js ichini o'zgartirdim

--- main3.js
+++ main3.js
@@ -5,7 +5,7 @@
 //         console.log(i);
 //     }
 // }
-
+//
 // for (let i = 0; i < a.length; i++) {
 //     for (let j = i + 1; j < a.length; j++) {
 //         if (a[i] < a[j]) {
@@ -15,9 +15,9 @@
 //         }
 //     }
 // }
-
+//
 // console.log(a);
-
+//
 // for (let i of a) {
 //     if (i % 2 == 0) {
 //         console.log(`${i} is even`);
@@ -26,9 +26,9 @@
 //         console.log(`${i} is odd`);
 //     }
 // }
-
+//
 // let b = 0
-
+//
 // while (b < 100) {
 //     if (b % 3 == 0 && b % 5 == 0) {
 //         console.log(`${b} is divisible by both 3 and 5`);
@@ -44,18 +44,18 @@
 
 // let son = prompt("Enter a number:");
 // let n = parseInt(son);
-// let b = n;               
+// let b = n;
 // let strong = 0;
-// let len = son.length;          
-
+// let len = son.length;
+//
 // while (b > 0) {
 //     let oxirgi = b % 10;
 //     strong += oxirgi ** len;
 //     b = Math.floor(b / 10);
 // }
-
+//
 // if (strong === n) {
-//     console.log("Bu son Armstrong son");
+//     console.log("Son Armstrong");
 // } else {
-//     console.log("Bu son Armstrong son emas");
+//     console.log("Son Armstrong emas");
 // }
