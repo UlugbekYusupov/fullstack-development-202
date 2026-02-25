1e3405a8af21fd2f09ae781c9ff44d1b58f945cf
Farruxbek <safarboyevfarruxbek56@gmail.com>
2026-02-25 10:57:04 +0500

    2-dars

---
+++ index.js
@@ -0,0 +1,28 @@
+// let list = [23,54,52,36,31,63,2,3,56,64,6,3]
+
+// let toq = 0
+// let juft = 0
+
+// for (let i = 0; i < list.length; i++) {
+//     if (list[i] % 2 == 0) {
+//         juft += 1
+//     } else {
+//         toq += 1
+//     }
+// }
+// console.log("Juft:", juft)
+// console.log("Toq:", toq)
+
+
+// let son = prompt("Son kiriting : ")
+// let teskari = son.reverse()
+// console.log("Result : ",teskari)
+
+// let katta = 234;
+// let a = Math.max(katta)
+// console.log("Max : ",a)
+
+let katta = 698;
+for (let i=0 ; i<katta.length(); i++) {
+    console.log(i)
+}
