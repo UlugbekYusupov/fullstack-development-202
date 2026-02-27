a276258ef2b00025d4374306b9787d4189dda652
Goldder12 <badiyvzamon007@gmail.com>
2026-02-27 18:51:04 +0500

    update new file

---
+++ main5.js
@@ -0,0 +1,31 @@
+
+// let a = 5
+//
+// function isPrime(a) {
+//     var sanoq = 0
+//     for (let i = 0; i <= a; i++) {
+//         if (a % i == 0) {
+//             sanoq += 1
+//         }
+//     }
+//     return sanoq
+// }
+//
+// let start = isPrime(a)
+// if (start == 2) {
+//     console.log(`${a} is prime`)
+// }
+// else {
+//     console.log(`${a} is not prime`)
+// }
+
+// function xonalarSoni(n) {
+//     let count = 0;
+//     do {
+//         count++;
+//         n = Math.floor(n / 10);
+//     } while (n > 0);
+//     return count;
+// }
+//
+// console.log(xonalarSoni(4567));
