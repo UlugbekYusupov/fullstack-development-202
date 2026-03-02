d9925c7fbbe7d7311de04b5e24925a92d0c6632d
b400xa30-spec <b400xa30@gmail.com>
2026-03-02 13:27:38 +0500

    test

--- 3-dars/3-dars.js
+++ 3-dars/3-dars.js
@@ -1,11 +1,6 @@
-let a = [1, 2, 3, 4, 5];
+function greet(name) {
+  return `Hello, ${name}!`;
+}
 
-for (let i of a) {
-    if (i % 2 == 0) {
-        console.log(`${i} juft son`);
-    }
-    else {
-        console.log(`${i} toq son`);
-    }
-}   
-
+// test
+console.log(greet("Ali "));
