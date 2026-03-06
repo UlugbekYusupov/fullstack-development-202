679fd19df27eba29ad3463d1466354f95c2cab20
Fazliddin <kfazliddin196@gmail.com>
2026-03-06 14:45:09 +0500

    JavaScript Arrays lesson

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-console.log("5" + 5);
-console.log("5" - 5);
-console.log("5" * 5);
-console.log("10" / 2);
-console.log(5 + true);
-console.log("10" - true);
-console.log("5" + true);
-console.log(5 + null);
-console.log(5 + undefined);
+function first(arr, n) {
+
+  if (n === undefined) {
+    return arr[0];
+  }
+
+  if (n < 0) {
+    return [];
+  }
+
+  return arr.slice(0, n);
+}
+
+// Misollar
+console.log(first([7, 9, 0, -2])); 
+console.log(first([], 0));
+console.log(first([7, 9, 0, -2], 3));
+console.log(first([7, 9, 0, -2], 6));
+console.log(first([7, 9, 0, -2], -3));
+
+function last(arr, n) {
+
+  if (n === undefined) {
+    return arr[arr.length - 1];
+  }
+
+  return arr.slice(Math.max(arr.length - n, 0));
+}
+
+// Misollar
+console.log(last([7, 9, 0, -2]));
+console.log(last([7, 9, 0, -2], 3));
+console.log(last([7, 9, 0, -2], 6));
+
+let myColor = ["Red", "Green", "White", "Black"];
+
+console.log(myColor.join());
