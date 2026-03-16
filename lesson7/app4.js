ed6c2ee7c23e76fe5f904a2b7b1498681cb258ac
BexruzHoshimov <hoshimovbehruz66@gmail.com>
2026-03-16 14:23:04 +0500

    vazifa yuklandi

--- lesson7/app4.js
+++ lesson7/app4.js
@@ -1,24 +1,24 @@
-const postlar = [
-  { author: "Alice", likes: 100, comments: 20, shares: 5 },
-  { author: "Bob", likes: 200, comments: 50, shares: 10 },
-];
+// const postlar = [
+//   { author: "Alice", likes: 100, comments: 20, shares: 5 },
+//   { author: "Bob", likes: 200, comments: 50, shares: 10 },
+// ];
 
-let engKopLike = 0;
-let engMashhurPost = "";
-let jamiFaollik = 0;
+// let engKopLike = 0;
+// let engMashhurPost = "";
+// let jamiFaollik = 0;
 
-for (i in postlar) {
-  let post = postlar[i];
+// for (i in postlar) {
+//   let post = postlar[i];
 
-  let engagement = post.likes + post.comments + post.shares;
+//   let engagement = post.likes + post.comments + post.shares;
 
-  jamiFaollik += engagement;
+//   jamiFaollik += engagement;
 
-  if (post.likes > engKopLike) {
-    engKopLike = post.likes;
-    engMashhurPost = post.author;
-  }
-}
+//   if (post.likes > engKopLike) {
+//     engKopLike = post.likes;
+//     engMashhurPost = post.author;
+//   }
+// }
 
-console.log("Eng kop like olgan post:", engMashhurPost);
-console.log("Jami engagement:", jamiFaollik);
+// console.log("Eng kop like olgan post:", engMashhurPost);
+// console.log("Jami engagement:", jamiFaollik);
--- lesson7/app6.js
+++ lesson7/app6.js
@@ -1,33 +1,33 @@
-const ombor = [
-  { itemName: "Laptop", category: "Electronics", stock: 5 },
-  { itemName: "Phone", category: "Electronics", stock: 10 },
-  { itemName: "Table", category: "Furniture", stock: 3 },
-];
+// const ombor = [
+//   { itemName: "Laptop", category: "Electronics", stock: 5 },
+//   { itemName: "Phone", category: "Electronics", stock: 10 },
+//   { itemName: "Table", category: "Furniture", stock: 3 },
+// ];
 
-function kategoriyaStock(category) {
-  let jami = 0;
+// function kategoriyaStock(category) {
+//   let jami = 0;
 
-  for (i in ombor) {
-    if (ombor[i].category === category) {
-      jami += ombor[i].stock;
-    }
-  }
+//   for (i in ombor) {
+//     if (ombor[i].category === category) {
+//       jami += ombor[i].stock;
+//     }
+//   }
 
-  return jami;
-}
+//   return jami;
+// }
 
-function kamStockAlert() {
-  let kamMahsulotlar = [];
+// function kamStockAlert() {
+//   let kamMahsulotlar = [];
 
-  for (i in ombor) {
-    if (ombor[i].stock < 5) {
-      kamMahsulotlar.push(ombor[i].itemName);
-    }
-  }
+//   for (i in ombor) {
+//     if (ombor[i].stock < 5) {
+//       kamMahsulotlar.push(ombor[i].itemName);
+//     }
+//   }
 
-  return kamMahsulotlar;
-}
+//   return kamMahsulotlar;
+// }
 
-console.log("Electronics jami stock:", kategoriyaStock("Electronics"));
+// console.log("Electronics jami stock:", kategoriyaStock("Electronics"));
 
-console.log("Kam qolgan mahsulotlar:", kamStockAlert());
+// console.log("Kam qolgan mahsulotlar:", kamStockAlert());
